35ee07cc161432993e62798b03502642e55bec1d
Kevinjr2912 <233372@ids.upchiapas.edu.mx>
2025-11-24 00:20:43 -0600

    config server 2

---
+++ backend/backend2.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { config } from './core/config';
+
+const app = express();
+const PORT = Number(config.PORT_SERVER) || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hola, mundo, desde el server 2');
+});
+
+app.listen(PORT,() => { console.log("Server running on http://localhost:"+PORT)});
